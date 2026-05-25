8a0d9d4d02387bd2e068257b6d31022b6a489af1
StenbuckDeng <Qiucan@gmail.com>
2026-05-25 22:09:27 +0800

    fix: update next.config serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.r2.cloudflarestorage.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.r2.dev",
-      },
+      { protocol: "https", hostname: "*.r2.cloudflarestorage.com" },
+      { protocol: "https", hostname: "*.r2.dev" },
     ],
   },
-  experimental: {
-    serverComponentsExternalPackages: ["@node-rs/argon2", "bcryptjs"],
-  },
+  serverExternalPackages: ["bcryptjs"],
 };
 
 export default nextConfig;
